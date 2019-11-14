2f4fd69dde439fcf94ced974443a2840fde46d53
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 20:09:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(array)
   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0 
+  array.map { "#{array[i]}
 end
 
 def long_planeteer_calls# code an argument here
