d549a01a7f244a733265da7431f5ba2b96c37979
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 19:50:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)
   i = 0 
   counter = 1 
   array.each_with_index { |name, index| }
-  puts "#{counter}. #{array[i]"
+  puts "#{counter}. #{array[i]}"
 end
 
 def summon_captain_planet# code an argument here
